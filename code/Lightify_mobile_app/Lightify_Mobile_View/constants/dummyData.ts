f8f2b492247304fe31abf81d9f92c7c904d5c737
e20262 <e20262@eng.pdn.ac.lk>
2025-04-24 11:52:05 +0530

    Create room WIP(Mostly)

--- code/Lightify_mobile_app/Lightify_Mobile_View/constants/dummyData.ts
+++ code/Lightify_mobile_app/Lightify_Mobile_View/constants/dummyData.ts
@@ -1,4 +1,5 @@
 export interface Device {
+    deviceID: number;
     deviceName: string;
     macAddress: string;
   }
@@ -6,18 +7,22 @@ export interface Device {
   
 export const devices: Device[] = [
     {
+        deviceID:1,
         deviceName: 'Device 1',
         macAddress: '00:11:22:33:44:55',
     },
     {
+        deviceID:2,
         deviceName: 'Device 2',
         macAddress: '66:77:88:99:AA:BB',
     },
     {
+        deviceID:3,
         deviceName: 'Device 3',
         macAddress: 'CC:DD:EE:FF:00:11',
     },
     {
+        deviceID:4,
         deviceName: 'Device 4',
         macAddress: '22:33:44:55:66:77',
     }
